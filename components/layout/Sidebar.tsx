d85c1a997dfa5da0eca7080b9ce0b74c9f893d57
Josh Sermon <jsermon@unknowna6a2f49b6b76.attlocal.net>
2023-05-15 23:15:42 -0400

    Added Logout Icon and Updated The Rest Of The Icons

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { FaUser } from 'react-icons/fa';
 import { BsHouseFill, BsBellFill } from 'react-icons/bs';
+import { BiLogOut } from 'react-icons/bi';
 import SidebarLogo from './SidebarLogo';
 import SidebarItem from './SidebarItem';
+import SidebarTweetButton from './SidebarTweetButton';
 
 
 const Sidebar = () => {
@@ -38,6 +40,9 @@ const Sidebar = () => {
                     icon={item.icon}
                     />
                 ))}
+
+                <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout" />
+                <SidebarTweetButton />
                 </div>
             </div>
         </div>
