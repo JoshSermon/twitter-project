9781a901e5ff03ae8612db681a4091adce8ee7a7
Josh Sermon <jsermon@unknowna6a2f49b6b76.attlocal.net>
2023-05-18 17:59:43 -0400

    Built Homepage repsonsive and started button

---
+++ components/Button.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+interface ButtonProps {
+    label: string;
+    secondary?: boolean;
+    fullWidth?: boolean;
+    large?: boolean;
+    onClick: () => void;
+    disabled?: boolean;
+    outline?: boolean;
+}
+
+const Button: React.FC<ButtonProps> = ({
+    label,
+    secondary,
+    fullWidth,
+    large,
+    onClick,
+    disabled,
+    outline
+
+}) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Button
