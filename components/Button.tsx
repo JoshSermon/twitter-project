a9b4b070b1c73adf527ae0b7e3de0ef0ca712f64
Josh Sermon <jsermon@unknowna6a2f49b6b76.attlocal.net>
2023-05-23 22:32:03 -0400

    Started Auth UI

--- components/Button.tsx
+++ components/Button.tsx
@@ -21,9 +21,25 @@ const Button: React.FC<ButtonProps> = ({
 
 }) => {
     return (
-        <div>
-            
-        </div>
+        <button 
+        disabled={disabled}
+        onClick={onClick}
+        className={`disabled:opacity-70 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 
+        ${fullWidth ? 'w-full' : 'w-fit'}
+        ${secondary ? 'bg-white' : 'bg-teal-600'}
+        ${secondary ? 'text-black' : 'text-white'}
+        ${secondary ? 'border-black' : 'bg-teal-600'}
+        ${large ? 'text-xl' : 'text-md'}
+        ${large ? 'px-5' : 'px-4'}
+        ${large ? 'py-3' : 'py-2'}
+        ${outline ? 'bg-transparent' : ''}
+        ${outline ? 'border-white' : ''}
+        ${outline ? 'text-white' : ''}
+        `
+
+        }>
+         {label}   
+        </button>
     )
 }
 
