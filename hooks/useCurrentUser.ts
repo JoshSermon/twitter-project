16e24c4923d1a8cdc673be3877c503127c6e80ad
Josh Sermon <jsermon@unknowna6a2f49b6b76.attlocal.net>
2023-06-10 23:21:43 -0400

    Updated CurrentUser component

--- hooks/useCurrentUser.ts
+++ hooks/useCurrentUser.ts
@@ -1,3 +1,16 @@
 import useSWR from 'swr';
 
+import fetcher from '../libs/fetcher';
+
+const useCurrentUser = () => {
+    const { data, error, isLoading, mutate } = useSWR('/api/current, fetcher');
+
+    return {
+        data,
+        error,
+        isLoading,
+        mutate
+    }
+};
+
 // Stopped at 1:36:00 - June 9th, 2023
